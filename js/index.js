0c0326ff170d1b9d0c8dfbf7e8cc79145520f3d9
CharlesLouis <contactokaou@gmail.com>
2018-08-26 10:17:31 +0200

    add a banner to buy the poster

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+
+	$('.buy-banner--wrapper--is-folded').find('.buy-banner--button').css({
+		'maxWidth' : $('.buy-banner--wrapper--is-folded').find('.buy-banner--button').outerWidth()
+	});
+
+    $('#buy-banner-toggle').on('click', function(e) {
+
+        $('#buy-banner').toggleClass('buy-banner--wrapper--is-folded buy-banner--wrapper--is-unfolded');
+        e.preventDefault();
+    });
+
     $(":checkbox").labelauty({
         label: false
     });
